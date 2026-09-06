99ad3bf0f3d500bea93945d15b4fd84c0d743dfe
DYART2026 <nelsonbohrer@dyart.com.br>
2026-09-06 11:37:22 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,19 @@
+// js/config.js - Configuração Central dos Municípios
+const CITY_CONFIGS = {
+  "friburgo": {
+    cityName: "Nova Friburgo",
+    folderName: "nova_friburgo",
+    gaMeasurementId: "G-XXXXXXXXXX_FRIBURGO", // Cole o ID real do GA4 de Friburgo
+    sheetUrl: "https://docs.google.com/spreadsheets/d/SEU_ID_PLANILHA_FRIBURGO/gviz/tq?",
+    defaultCoords: [-22.2818, -42.5306],
+    logo: "assets/logos/logo-friburgo.png"
+  },
+  "rio": {
+    cityName: "Rio de Janeiro",
+    folderName: "rio_de_janeiro",
+    gaMeasurementId: "G-XXXXXXXXXX_RIO", // Cole o ID real do GA4 do Rio
+    sheetUrl: "https://docs.google.com/spreadsheets/d/SEU_ID_PLANILHA_RIO/gviz/tq?",
+    defaultCoords: [-22.9068, -43.1729],
+    logo: "assets/logos/logo-rio.png"
+  }
+};
