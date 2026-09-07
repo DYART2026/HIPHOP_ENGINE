c521de3e3edf2aa378932104b1910f0fe2106ddd
DYART2026 <nelsonbohrer@dyart.com.br>
2026-09-07 19:06:37 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,14 +3,14 @@ const CITY_CONFIGS = {
   "friburgo": {
     cityName: "Nova Friburgo",
     folderName: "nova_friburgo",
-    gaMeasurementId: "G-XXXXXXXXXX_FRIBURGO", // Cole o ID real do GA4 de Friburgo
+    gaMeasurementId: "G-TCZK8ZGF7V", // Cole o ID real do GA4 de Friburgo
     sheetUrl: "https://docs.google.com/spreadsheets/d/1wDoBV28qzHMrj4_yz-gsLPbtBsyYvMcvKS-jlzjuMi8/gviz/tq?",
     defaultCoords: [-22.2818, -42.5306],
   },
   "rio": {
     cityName: "Rio de Janeiro",
     folderName: "rio_de_janeiro",
-    gaMeasurementId: "G-XXXXXXXXXX_RIO", // Cole o ID real do GA4 do Rio
+    gaMeasurementId: "G-CSC7E5E5QC", // Cole o ID real do GA4 do Rio
     sheetUrl: "https://docs.google.com/spreadsheets/d/1oxeXlXTxybVMNYWzauCfp_GATkFzVoXqpTtvKKmmkHg/gviz/tq?",
     defaultCoords: [-22.9068, -43.1729],
   }
