9a8c7b765a0b0ac7dbe11ae1724a1c66f9b72dd5
DYART2026 <nelsonbohrer@dyart.com.br>
2026-09-15 13:33:20 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ const CITY_CONFIGS = {
     cityName: "Rio de Janeiro",
     folderName: "rio_de_janeiro",
     gaMeasurementId: "G-CSC7E5E5QC", // Cole o ID real do GA4 do Rio
-    sheetUrl: "https://docs.google.com/spreadsheets/d/1oxeXlXTxybVMNYWzauCfp_GATkFzVoXqpTtvKKmmkHg/gviz/tq?",
+    sheetUrl: "https://docs.google.com/spreadsheets/d/1K_xyd--TX3TmKWgJRu6Du2TG_kQqrmHXsVBRr9Z1E08/gviz/tq?",
     defaultCoords: [-22.9068, -43.1729],
   }
 };
