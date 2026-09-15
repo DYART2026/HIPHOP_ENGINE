314deaae4a49d2476064125fd396711fa44af571
DYART2026 <nelsonbohrer@dyart.com.br>
2026-09-15 12:29:10 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,5 +13,12 @@ const CITY_CONFIGS = {
     gaMeasurementId: "G-CSC7E5E5QC", // Cole o ID real do GA4 do Rio
     sheetUrl: "https://docs.google.com/spreadsheets/d/1oxeXlXTxybVMNYWzauCfp_GATkFzVoXqpTtvKKmmkHg/gviz/tq?",
     defaultCoords: [-22.9068, -43.1729],
+  },
+  "ipanema": {
+    cityName: "Rio de Janeiro",
+    folderName: "rio_de_janeiro",
+    gaMeasurementId: "G-CSC7E5E5QC", // Cole o ID real do GA4 do Rio
+    sheetUrl: "https://docs.google.com/spreadsheets/d/1oxeXlXTxybVMNYWzauCfp_GATkFzVoXqpTtvKKmmkHg/gviz/tq?",
+    defaultCoords: [-22.9068, -43.1729],
   }
 };
